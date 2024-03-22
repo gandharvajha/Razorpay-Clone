9982ab9706e85307abeeb537aba1be7fa4637e00
gandharvajha <gandharva1371998@gmail.com>
2024-03-22 23:21:00 +0530

    First Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       "fontFamily":{
-        mulish:["Mulish","san-serif"],
+        mullish:["Mulish","san-serif"],
       },
       color:{
         deepBlue:"#02042a",
